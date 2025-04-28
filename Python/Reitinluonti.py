8ada0fcd249390d57c2cba3e1448240dfef646bd
ronim <roni.minkkinen@gmail.com>
2025-04-28 21:13:17 +0300

    Masteri ja reitinluonti appi luotu

--- Python/Reitinluonti.py
+++ Python/Reitinluonti.py
@@ -69,6 +69,7 @@ def page_not_found(virhekoodi): #virheenhändläys
         "status" : "404",
         "teksti" : "Virheellinen päätepiste"
     }
+
     jsonvast = json.dumps(vastaus)
     return Response(response=jsonvast, status=404, mimetype="application/json")
 if __name__ == '__main__': #pyörittää appia
