f24a36b61352f68c31c77668822f4fc52e63e46c
Ousalou <osalonen@icloud.com>
2025-05-04 15:31:06 +0300

    hahmonluontia

--- outin_testit/mainmenu-hahmonluonti.py
+++ outin_testit/mainmenu-hahmonluonti.py
@@ -20,7 +20,7 @@ flight_game_backend_app_OUTI = Flask(__name__, template_folder='Python/templates
 def new_game():
     return render_template("new_game.html")
 
-#@flight_game_backend_app_OUTI.route('/old_user')
+@flight_game_backend_app_OUTI.route('/old_user')
 def old_users_fetch():
     sql = f"select screen_name from game;"
     cursor = yhteys.cursor()
@@ -30,14 +30,14 @@ def old_users_fetch():
     users_list = []                     #TÄMÄ FOR-LOOP, KOSKA TULEE MUUTEN TUPLENA > vaikea käsitellä > muunnetaan listaksi
     for user in users:
         users_list.append(user)
-    return jsonify(users_list)
+    return json.dumps(users_list)           #muutettu jsonify > json dumps
 
 def create_new_user():
-    #ottaa inputin frontista
+    new_user = request.from[]                         #ottaa inputin frontista
     #hakee
     pass                                                                      #front-endiin printti! >> OMAT NAPIT OLEMASSA OLEVILLE >> get seuraavaan?
 
-# @flight_game_backend_app_OUTI.route('/old_user/<user>')
+@flight_game_backend_app_OUTI.route('/old_user/<user>')
 def get_user(user):
     sql = f"select * from game where screen_name = '{user}';"
     cursor = yhteys.cursor()
@@ -47,36 +47,9 @@ def get_user(user):
     if user:
         return jsonify(user_full_info)
     if not user:
-        page_not_found()
+        pass #404
 
-@flight_game_backend_app_OUTI.errorhandler(404) #TUPLA! POISTA IN CASE
-def page_not_found(virhekoodi):
-    vastaus = {
-        "status" : "404",
-        "teksti" : "Virheellinen päätepiste"
-    }
-    jsonvast = json.dumps(vastaus)
-    return Response(response=jsonvast, status=404, mimetype="application/json")
 
 if __name__ == '__main__':
     flight_game_backend_app_OUTI.run(use_reloader=True, host='127.0.0.1', port=3000)
-
-#    global option
-#    global user
-#
- #   while True:
-  #      #Looppaa main menuun kunnes pelaaja haluaa alottaa uuden pelin
-   #     print("-------------------------\n")
-    #    print(f"\n>New Game\n>Scoreboard\n>Rules\n>Quit\n")
-     #   option = input("Please select >").lower()
-      #  if option == "new game" or option == "new":
-       #     user = new_game()
-        #    break
-        #elif option == "scoreboard":
-           # scoreboard()
-        #elif option == "rules":
-     #       instructions()
-      #  elif option == "quit":
-       #     quit()
-   # return user
 
