9c293eba2301e2c9b2dbf30cbb6ceecbb32e0c73
sonjameskus <sonja.meskus@metropolia.fi>
2025-05-08 12:48:24 +0300

    new user

--- Python/static/new_game.js
+++ Python/static/new_game.js
@@ -4,7 +4,7 @@ document.getElementById("old-user-btn").addEventListener("click", () => {
   window.location.href = "/new_game/old_user"; //vanha käyttäjä
 });
 document.getElementById("new-user-btn").addEventListener("click", () => {
-  window.location.href = "/new_user"; //uusi käyttäjä
+  window.location.href = "/new_game/new_user"; //uusi käyttäjä
 });
 
 document.getElementById("quit-btn").addEventListener("click", () => {
