b7fc33a20d58f1b93e50bac03f48e715a15bf0da
sonjameskus <sonja.meskus@metropolia.fi>
2025-05-08 11:59:31 +0300

    old user

---
+++ Python/static/old_user.js
@@ -0,0 +1,28 @@
+'use strict'
+
+fetch("/new_game/old_user")
+    .then(response => response.json())
+    .then(data => {
+        const insideTable = document.getElementById('insideTable');
+        data.forEach(username => {
+            const button = document.createElement('button');
+            button.textContent = username;
+            button.className = 'username-button';
+
+            button.addEventListener('click', () => {
+                window.location.href = `/new_game/old_user/${username}`;
+            });
+
+            insideTable.appendChild(button);
+        });
+    });
+
+document.getElementById("new_game-btn").addEventListener("click", () => {
+  window.location.href = "/new_game";
+});
+document.getElementById("main_menu-btn").addEventListener("click", () => {
+  window.location.href = "/main_menu";
+});
+document.getElementById("quit-btn").addEventListener("click", () => {
+  window.location.href = "/start";
+});
